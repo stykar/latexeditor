7e717a2215886c3718334075f2cfbb9b33d6d374
stykar <stekarageorgis@gmail.com>
2019-03-26 20:56:32 +0200

    us-1

--- latexeditor.java
+++ latexeditor.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class latexeditor{
      public static void main(String[] args) {
@@ -8,26 +9,81 @@ public class latexeditor{
         System.out.println("- 2 for a book template");
         System.out.println("- 3 for an article template");
         System.out.println("- 4 for a letter template");
+        System.out.print("Input:");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println("");
         String line="";
         String text="";
         BufferedReader src;
         FileWriter dest;
-        try {
-            src = new BufferedReader(new FileReader("temp1.txt"));
-            dest = new FileWriter("test.tex");
-            line=src.readLine();
-            while (line != null) {
-                dest.write(line+"\n");
-                System.out.println(line);
+        if(num == 1){
+            try {
+                src = new BufferedReader(new FileReader("temp1.txt"));
+                dest = new FileWriter("template.tex");
                 line=src.readLine();
+                while (line != null) {
+                    dest.write(line+"\n");
+                    line=src.readLine();
+                }
+                src.close();
+                dest.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+
             }
-            System.out.println(text);
-            src.close();
-            dest.close();
-        } catch (FileNotFoundException e) {
+        }
+        if(num == 2){
+            try {
+                src = new BufferedReader(new FileReader("temp2.txt"));
+                dest = new FileWriter("template.tex");
+                line=src.readLine();
+                while (line != null) {
+                    dest.write(line+"\n");
+                    line=src.readLine();
+                }
+                src.close();
+                dest.close();
+            } catch (FileNotFoundException e) {
 
-        } catch (IOException e) {
+            } catch (IOException e) {
 
+            }
+        }
+        if(num == 3){
+            try {
+                src = new BufferedReader(new FileReader("temp3.txt"));
+                dest = new FileWriter("template.tex");
+                line=src.readLine();
+                while (line != null) {
+                    dest.write(line+"\n");
+                    line=src.readLine();
+                }
+                src.close();
+                dest.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+
+            }
+        }
+        if(num == 4){
+            try {
+                src = new BufferedReader(new FileReader("temp4.txt"));
+                dest = new FileWriter("template.tex");
+                line=src.readLine();
+                while (line != null) {
+                    dest.write(line+"\n");
+                    line=src.readLine();
+                }
+                src.close();
+                dest.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+
+            }
         }
     }
 }
